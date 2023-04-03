42a147c106461ede1654023485780b03a2bbc800
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-04-03 14:02:03 +0200

    1.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="A compact Python package for handling ArUCo markers.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="1.0.2",
+    version="1.1.0",
     author="Christopher E. Mower",
     author_email="christopher.mower@kcl.ac.uk",
     url="https://github.com/cmower/aruco_markers",
